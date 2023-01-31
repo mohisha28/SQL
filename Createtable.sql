164a4cd5ea0af9bb050950491f88cc55dccea038
Mohisha Gunturu <98393079+mohisha28@users.noreply.github.com>
2023-01-31 21:03:06 +0530

    Create Createtable.sql

---
+++ Createtable.sql
@@ -0,0 +1,12 @@
+## BASICS 
+
+create table A1(
+  student_id int,
+  name varchar(20),
+  major varchar(20),
+  primary key(student_id)
+  );
+insert into A1 values(1,'Jack','biology'),(2,'Sam','history'),(3,'Ayan','maths'),(4,'Rohan','physics');
+select * from A1;
+
+
